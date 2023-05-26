27979be996a0262697208fb6c35ab48d74254fe3
BradleyAPierce <bradleyapierce@me.com>
2023-05-26 11:01:40 -0700

    Commit

---
+++ dmdraganddropconnectactivity/script.js
@@ -0,0 +1,59 @@
+$( function() {
+    $( "#customerdraggable" ).draggable();
+  });
+
+$( function() {
+    $( "#strategydraggable" ).draggable();
+  });
+
+$( function() {
+    $( "#technologydraggable" ).draggable();
+  });
+
+$( function() {
+    $( "#operationsdraggable" ).draggable();
+  });
+
+$( function() {
+    $( "#organizationdraggable" ).draggable();
+  });
+
+$( function() {
+    $( "#customerdraggablecurrent" ).draggable();
+  });
+
+$( function() {
+    $( "#strategydraggablecurrent" ).draggable();
+  });
+
+$( function() {
+    $( "#technologydraggablecurrent" ).draggable();
+  });
+
+$( function() {
+    $( "#operationsdraggablecurrent" ).draggable();
+  });
+
+$( function() {
+    $( "#organizationdraggablecurrent" ).draggable();
+  });
+
+$( function() {
+    $( "#customerdraggablefuture" ).draggable();
+  });
+
+$( function() {
+    $( "#strategydraggablefuture" ).draggable();
+  });
+
+$( function() {
+    $( "#technologydraggablefuture" ).draggable();
+  });
+
+$( function() {
+    $( "#operationsdraggablefuture" ).draggable();
+  });
+
+$( function() {
+    $( "#organizationdraggablefuture" ).draggable();
+  });
